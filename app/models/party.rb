6c3dc8fe3792f8083442af494182726465c2f5e8
Arnaud Floriani <sarkraf.dev@gmail.com>
2024-10-28 16:57:33 +0100

    parties validates

--- app/models/party.rb
+++ app/models/party.rb
@@ -1,2 +1,12 @@
 class Party < ApplicationRecord
+  validates :name, presence: true, uniqueness: true, length: { maximum: 25, minimum: 3 }
+  validates :passphrase, presence: true
+  validates :url, presence: true, uniqueness: true
+
+  before_validation :generate_url
+
+  def generate_url
+    url = SecureRandom.alphanumeric(10)
+    Party.exists?(url:) ? generate_url : url
+  end
 end
