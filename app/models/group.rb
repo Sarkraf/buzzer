39d56d45e05c85565b2c91e559adc3db3443d7a1
Alexis Reubrecht <lxsrbrcht@gmail.com>
2024-10-28 17:09:29 +0100

    group model upgrade + default false on buzzers

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,3 +1,12 @@
 class Group < ApplicationRecord
   belongs_to :party
+
+  validates :name, presence: true, length: { minimum: 2, maximum: 30 }, uniqueness: { scope: :party_id }
+  validates :score, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
+  before_validation :set_score_to_zero
+
+  def set_score_to_zero
+    self.score = 0
+  end
 end
---
+++ db/migrate/20241028155100_set_default_buzz.rb
@@ -0,0 +1,5 @@
+class SetDefaultBuzz < ActiveRecord::Migration[7.1]
+  def change
+    change_column_default :buzzs, clicked: false
+  end
+end
