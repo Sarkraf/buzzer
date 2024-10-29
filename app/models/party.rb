2275af47876877f02952ddf302a8e5df79063dc0
Arnaud Floriani <sarkraf.dev@gmail.com>
2024-10-29 14:52:25 +0100

    add depencies for all models

--- app/models/party.rb
+++ app/models/party.rb
@@ -1,4 +1,6 @@
 class Party < ApplicationRecord
+  has_many :groups, dependent: :destroy
+
   validates :name, presence: true, uniqueness: true, length: { maximum: 25, minimum: 3 }
   validates :passphrase, presence: true
   validates :url, presence: true, uniqueness: true
