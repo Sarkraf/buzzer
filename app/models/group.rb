a1c4b73600b732a2fc39e9bc39ed76412305df8b
Arnaud Floriani <sarkraf.dev@gmail.com>
2024-10-29 14:50:28 +0100

    fix validates, add auto creation for buzzer and add method for update score

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,12 +1,23 @@
 class Group < ApplicationRecord
   belongs_to :party
+  has_one :buzz, dependent: :destroy
 
   validates :name, presence: true, length: { minimum: 2, maximum: 30 }, uniqueness: { scope: :party_id }
-  validates :score, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :score, presence: true, numericality: { greater_than_or_equal_to: 0 }
 
-  before_validation :set_score_to_zero
+  before_validation :set_score_to_zero, if: :new_record?
+  after_create :create_buzz
 
   def set_score_to_zero
     self.score = 0
+  end
+
+  def update_score(operator, score)
+    self.score = self.score.send(operator, score)
+    save
+  end
+
+  def create_buzz
+    Buzz.create(group: self)
   end
 end
