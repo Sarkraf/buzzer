32ea4de8d36d9364f25808fead128ac4e1890719
Arnaud Floriani <sarkraf.dev@gmail.com>
2024-10-30 12:07:16 +0100

    add show and group_params

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,4 +1,22 @@
 class GroupsController < ApplicationController
   def create
+    @group = Group.new(group_params)
+    if @group.save
+      redirect_to party_group_path(@group.party, @group)
+    else
+      @party = @group.party
+      render "parties/choice", status: :unprocessable_entity
+    end
+  end
+
+  def show
+    @group = Group.find_by(name: params[:name])
+    @buzz = @group.buzz
+  end
+
+  private
+
+  def group_params
+    params.require(:group).permit(:name, :party_id)
   end
 end
